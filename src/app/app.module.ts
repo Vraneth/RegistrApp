9735832cc7b6fcf831a87bb8a2f3fd501d39b0f1
Valparaiso <valparaiso@duoc.acad>
2024-11-05 08:44:29 -0300

    Proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { CamaraAlumnoComponent } from './camara-alumno/camara-alumno.component';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CodigoqrComponent } from './codigoqr/codigoqr.component';
+import { ReglamentoProfesorComponent } from './reglamento-profesor/reglamento-profesor.component';
+import { AlumnoPerfilComponent } from './alumno-perfil/alumno-perfil.component';
+import { ProfesorPerfilComponent } from './profesor-perfil/profesor-perfil.component';
+import { RecuperarContrasenaComponent } from './recuperar-contrasena/recuperar-contrasena.component';
+import { FormsModule } from '@angular/forms';
+import { HistorialClasesComponent } from './historial-clases/historial-clases.component';
+import { HistorialAsistenciaComponent } from './historial-asistencia/historial-asistencia.component'
+import { CrearClaseComponent } from './crear-clase/crear-clase.component';
+import { CrearUsuarioComponent } from './crear-usuario/crear-usuario.component';
+
+
+@NgModule({
+  declarations: [AppComponent, CodigoqrComponent, ReglamentoProfesorComponent,AlumnoPerfilComponent,CamaraAlumnoComponent, ProfesorPerfilComponent, RecuperarContrasenaComponent, HistorialClasesComponent, HistorialAsistenciaComponent, CrearClaseComponent, CrearUsuarioComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, RouterModule, HttpClientModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/reglamento-profesor/reglamento-profesor.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-reglamento-profesor',
+  templateUrl: './reglamento-profesor.component.html',
+  styleUrls: ['./reglamento-profesor.component.scss'],
+})
+export class ReglamentoProfesorComponent  implements OnInit {
+
+  constructor(private router: Router) { }
+
+  regresarALogin() {
+      this.router.navigate(['/login']); 
+    }
+
+  ngOnInit() {}
+
+}
